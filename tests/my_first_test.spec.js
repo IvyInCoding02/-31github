a815ee48e5f83dfdb29a2ac982ca5aebc8105a46
Aibiike <aibiikeabdysamatova@gmail.com>
2023-11-07 23:57:32 +0600

    writing first test

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -1,1 +1,8 @@
 const { test, expect } = require('@playwright/test');
+
+
+// First Testing Practice
+// const {hello, helloworld} = require('./demo/hello');
+// import {hello,helloworld} from './demo/hello';
+// console.log(hello());
+// console.log(helloworld());
