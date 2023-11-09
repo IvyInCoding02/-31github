a5847d5ceb73628c7a1dbd6de24eafb365132fd4
Aibiike <aibiikeabdysamatova@gmail.com>
2023-11-09 23:58:02 +0600

    change the data for testing and add playwright hooks

--- tests/demo/record2_experiment.spec.js
+++ tests/demo/record2_experiment.spec.js
@@ -8,7 +8,7 @@ test('record demo 2', async () => {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('https://www.google.com/search?q=github&oq=github+&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCTEyMDM2ajBqMqgCALACAA&sourceid=chrome&ie=UTF-8');
-  await page.getByRole('link', { name: 'GitHub: Let\'s build from here · GitHub GitHub https://github.com' }).click();
+  await page.getByRole('link', { name: 'GitHub: Let\'s build from here · GitHub GitHub https://github123.com' }).click();
   await page.close();
 
   // ---------------------
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,14 +1,39 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-(async ({ page }) => {
+let context;
+let page;
+test.beforeAll(async({browser}) =>{
+  context = await browser.newContext()
+  await context.tracing.start({
+    snapshots: true,
+    screenshots: true
+  })
+  page = await context.newPage()
+})
+
+test.afterAll(async() => {
+  await context.tracing.stop({
+    path: 'test2_trace.zip'
+  })  
+})
+
+test('homepage', async ({}) => {
+  // await context.tracing.start
+  // ({ snapshots: true, 
+  //   screenshots: true 
+  // }); 
   await page.goto('https://playwright.dev/');
+  // await context.tracing.stop({
+  //   path: 'test1_trace.zip'
+  // })
+
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started link', async ({ page}) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
