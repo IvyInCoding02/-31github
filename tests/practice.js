c282eaed400b68ee06a6421eebfef7c2044da7ad
Aibiike <aibiikeabdysamatova@gmail.com>
2023-11-12 20:16:23 +0600

    study adaption of playwright in different browsers

--- tests/practice.js
+++ tests/practice.js
@@ -2,10 +2,14 @@ const {webkit, chromium, firefox} = require("playwright");
 
 (async () => {
     for (const browserType of [webkit,firefox, chromium]){
-        const browser = await browserType.launch();
-        const page = await browser.newPage();
-        await page.goto("")
-    }
-    
+        const browser = await browserType.launch();  //used to lauch each browser 
+        const page = await browser.newPage();        //used to open the page
+        await page.goto("https://www.wildberries.ru/")      // of the current page
+        await page.screenshot({
+            path: `screenshot${browserType.name()}.png`,  //save the screenshot 
+        })
+        await browser.close(); //close the screenshot 
 
+        console.log("success" + browserType.name())  //control the progress
+    }
 })
