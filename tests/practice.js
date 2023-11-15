2813b90c66ebafa5cc2cf58415603575da155dd3
Aibiike <aibiikeabdysamatova@gmail.com>
2023-11-15 22:33:09 +0600

    complete screenshot task for the website

--- tests/practice.js
+++ tests/practice.js
@@ -13,3 +13,17 @@ const {webkit, chromium, firefox} = require("playwright");
         console.log("success" + browserType.name())  //control the progress
     }
 })
+
+(async () => {
+    for (const browserType of [webkit,firefox, chromium]){
+        const browser = await browserType.launch();  //used to lauch each browser 
+        const page = await browser.newPage();        //used to open the page
+        await page.goto("https://www.wildberries.ru/")      // of the current page
+        await page.screenshot({
+            path: `screenshot${browserType.name()}.png`,  //save the screenshot 
+        })
+        await browser.close(); //close the screenshot 
+
+        console.log("success" + browserType.name())  //control the progress
+    }
+})
