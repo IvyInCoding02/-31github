29be70f15ae62f9b863a519bc6545d88bca51b2e
Aibiike <aibiikeabdysamatova@gmail.com>
2023-11-18 23:35:29 +0600

    study playwright screenshot methods

--- tests/practice2.js
+++ tests/practice2.js
@@ -1,13 +1,39 @@
-const { webkit, devices } = require('playwright');
-const iPhone = devices['iPhone 6'];
+// const { chromium } = require('playwright');
+
+
+// (async () => {
+//   const browser = await chromium.launch();
+//   const context = await browser.newContext({
+//     ...iPhone
+//   });
+//   const page = await context.newPage();
+//   await page.goto('https://www.wildberries.ru/catalog/189920751/detail.aspx?targetUrl=SP');
+
+//   await browser.close();
+// });
+
+// const { webkit } = require('playwright');
+
+// (async () => {
+//   const browser = await webkit.launch({headless: false, slowMo: 50 });
+//   const page = await browser.newPage();
+//   await page.goto('https://playwright.dev/');
+//   await page.screenshot({ path: `example.png` });
+//   await browser.close();
+// });
+
+const { webkit,chromium, devices } = require('playwright');
+const deviceType = devices['Desktop Chrome'];
 
 (async () => {
-  const browser = await webkit.launch();
-  const context = await browser.newContext({
-    ...iPhone
-  });
-  const page = await context.newPage();
-  await page.goto('https://www.wildberries.ru/catalog/189920751/detail.aspx?targetUrl=SP');
-  // other actions...
-  await browser.close();
-});
+   const browser = await chromium.launch({headless: false});
+   const context = await browser.newContext({});
+   console.log(deviceType);
+   const page = await context.newPage();
+   await page.goto('https://www.wildberries.ru/')
+   await page.screenshot({
+     path: `screenshots-${deviceType.name}.png`,
+     fullPage: true,
+   });
+//  await browser.close();
+})();
