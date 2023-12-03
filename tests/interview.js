9b9b4c41bfcc73857b320041df747388ebfc1b32
Aibiike <aibiikeabdysamatova@gmail.com>
2023-12-03 23:24:12 +0600

    technical interview questions

--- tests/interview.js
+++ tests/interview.js
@@ -6,3 +6,13 @@ console.log(sessionStorage.getItem('username')); // выводит "John
 // 
 localStorage.setItem('username', 'John');
 console.log(localStorage.getItem('username')); // выводит "John"
+
+let arr = [1, 2, 2, 3, 2, 3, 4, 5, 4, 1, 4, 5];
+let output = [...new Set(arr)];
+console.log(output); //[1, 2, 3, 4, 5]
+
+let arr = [1, 2, 2, 3, 2, 3, 4, 5, 4, 1, 4, 5];
+let result = arr.filter((item, index) => {
+  return arr.indexOf(item) == index;
+});
+console.log(result); //[1, 2, 3, 4, 5]
