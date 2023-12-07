abec4ec8587a2a643d338e7bf6413406f3a2c8e5
Aibiike <aibiikeabdysamatova@gmail.com>
2023-12-08 01:29:23 +0600

    interview preparation

--- tests/interview.js
+++ tests/interview.js
@@ -3,3 +3,9 @@
 // 3. What are the differences between HTML and XHTML?
 // 4. What is responsive design?
 // 5. What are the different types of CSS selectors?
+
+//Links to articles and resources to prepare for the interview
+// https://tproger.ru/articles/frontend-roadmap-2 - roadmap(must be read)
+// https://tproger.ru/articles/20-voprosov-na-sobesedovanii-frontend-razrabotchika(done with reading)
+// https://tproger.ru/articles/20-voprosov-na-sobesedovanii-frontend-razrabotchika(tech questions to be read and solved)
+//d3.js and chart.js(must be researched)
