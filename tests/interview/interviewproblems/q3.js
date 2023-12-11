a3e367302998b56ac18f9698b2db4549b45da4a9
Aibiike <aibiikeabdysamatova@gmail.com>
2023-12-11 23:06:18 +0600

    q3 to solve

---
+++ tests/interview/interviewproblems/q3.js
@@ -0,0 +1,17 @@
+// Panic function
+//Write a PANIC! function. The function should 
+//take in a sentece and return a sentence with all
+//capital letters and exclamation mark(!) at the end.
+//Use JS bult in string methods.
+
+//If the string is a phrase or a sentece, add 
+//an emoji between each word.
+
+//Example input: "Hello"
+//Example output: "HELLO!"
+
+//Example input: I am almost out of coffee
+//Example output: I (emoji) AM (emoji) ALMOST (emoji) OUT (emoji) OF (emoji) COFFEE.
+
+//CLUE 
+// .split() .join();
