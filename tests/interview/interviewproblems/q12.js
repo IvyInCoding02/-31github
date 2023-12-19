1b063ae6b5492b5f7638b93da19bdbaa2495cb77
Aibiike <aibiikeabdysamatova@gmail.com>
2023-12-20 00:38:09 +0600

    solve q13 on checking the amount of the characters

--- tests/interview/interviewproblems/q12.js
+++ tests/interview/interviewproblems/q12.js
@@ -14,19 +14,30 @@ Example output: "abc12"
 
 const password = "9338dsabbbadjdjdj2sdfdfdf282ff8fdsd888ss8cfgfg332q23"; 
 
+//REPEAT SOLVING PROBLEM ON MY OWN
 function removeDupeChars(chars){
-    //create a new, empty string dupesRemoved
-    let dupesRemoved = "";
-    //loop through the string we want to remove dupes form 
-    for(let i = 0; i < chars.length; i++){
-        //for every character in the string, check is it dupesRemoved?
-        if(!dupesRemoved.includes(chars[i])){
-            //if not, add it 
-            dupesRemoved += chars[i]
-        }
+let dupesRemoved = "";
+for(let i = 0; i < chars.length; i++){
+    if(!dupesRemoved.includes(chars[i])){
+        dupesRemoved += chars[i]
     }
-     // dupesRemoved -- it has no duplicates!
-    return dupesRemoved;
 }
+return dupesRemoved;
+}
+
+// function removeDupeChars(chars){
+//         //create a new, empty string dupesRemoved
+//         let dupesRemoved = "";
+//         //loop through the string we want to remove dupes form 
+//         for(let i = 0; i < chars.length; i++){
+//             //for every character in the string, check is it dupesRemoved?
+//             if(!dupesRemoved.includes(chars[i])){
+//                 //if not, add it 
+//                 dupesRemoved += chars[i]
+//             }
+//         }
+//          // dupesRemoved -- it has no duplicates!
+//         return dupesRemoved;
+// }
 
 console.log(removeDupeChars(password));
